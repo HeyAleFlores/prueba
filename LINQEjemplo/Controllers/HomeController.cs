b9704081f2d67b3b01f7ca8e74e0b102ee6d111b
unknown <SPUNIVO4@SPUNIVO4.sv.cds>
2018-12-10 10:37:20 -0600

    Proyecto

---
+++ LINQEjemplo/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LINQEjemplo.Models;
+
+namespace LINQEjemplo.Controllers
+{
+    public class HomeController : Controller
+    {
+        Registros R  = new Registros();
+        // GET: Home
+        public ActionResult Index()
+        {
+
+            var Listado = from p in R.Alumnos()
+                          select p;
+           
+            return View(Listado);
+        }
+        [HttpPost]
+        public ActionResult Index(string Busqueda,string filtro )
+        {
+            var Listado = from p in R.Alumnos()
+                          select p;
+
+            switch (filtro)
+                {
+                case "Materia":
+                    var Listado1 = from p in R.Alumnos()
+                                  where p.Materia.Contains(Busqueda)
+                                  select p;
+
+                    return View(Listado1);
+
+                case "Nombre":
+                    var Listado2 = from p in R.Alumnos()
+                                   where p.Nombre.Contains(Busqueda)
+                                   select p;
+
+                    return View(Listado2);  
+            }
+
+            return View(Listado);
+        }
+    }
+}
---
+++ LINQEjemplo/Models/Registros.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LINQEjemplo.Models
+{
+    public class Registros
+    {
+        public string Carnet { get; set; }
+        public string Nombre { get; set; }
+        public string Materia { get; set; }
+        public int Promedio { get; set; }
+        public string  Fecha { get; set; }
+
+        public List<Registros> Alumnos()
+        {
+            var alumno1 = new Registros
+            {
+                Carnet = "CB17002",
+                Nombre = "Angel",
+                Materia = "Matemática",
+                Promedio = 10,
+                Fecha = "12/08/2018"
+            };
+
+            var alumno2 = new Registros
+            {
+                Carnet = "FA13012",
+                Nombre = "Josue",
+                Materia = "Ciencias",
+                Promedio = 8,
+                Fecha = "12/09/2017"
+            };
+
+            var alumno3 = new Registros
+            {
+                Carnet = "GF20893",
+                Nombre = "Juana",
+                Materia = "Sociales",
+                Promedio = 6,
+                Fecha = "15/09/2017"
+            };
+
+            return new List<Registros>(){
+                alumno1, alumno2, alumno3
+            };
+        }
+    }
+}
